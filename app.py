4dbfc4337020486f1f5a19d8b59f902c56cc31df
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-06-11 12:36:55 +0300

    Change Ports

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def edit_task(task_id):
     return jsonify({'id': task.id, 'title': task.title})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
