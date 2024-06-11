8a860099510ced1031e19812b9df18d9a31a1ece
WINNIE-MBINYA <winnymbinyamwanza@gmail.com>
2024-06-11 12:42:45 +0300

    Change Ports

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import date, datetime
 from collections import defaultdict
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
